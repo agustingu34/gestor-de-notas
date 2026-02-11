4d66c4c5b9be6ef32cc0281d9d9664950b37aebe
agustingu34 <agus62gutierrez@gmail.com>
2026-02-11 00:17:55 -0300

    V2: Agregado borrar ultima nota y validaciones

--- gestor_de_notas.py
+++ gestor_de_notas.py
@@ -1,12 +1,21 @@
 notas=[]
+try:
+    with open("notas.txt", "r") as f:
+        for linea in f:              
+            notas.append(int(linea.strip()))
+except:
+    pass
 while True:
     print("\n1 - Agregar")
     print("2 -Ver notas")
     print("3 - Promedio")
     print("4 - Borrar ultima nota")
-    print("5 - Salir")
+    print("5 - Guardar al Salir")
     usuario=int(input("Elija una opcion: "))
     if usuario==5:
+        with open("notas.txt","w") as f:
+            for nota in notas:
+                f.write(str(nota)+ "\n")
         print("fin del programa.")
         break
     elif usuario==1:
@@ -24,6 +33,6 @@ while True:
     elif usuario==4:
         if notas:
             notas.pop()
-            print("Se elimino la ultima nota agregada")
+            print("Se elimino la ultim anota agregada")
         elif len(notas)==0:
             print("no hay notas.")
